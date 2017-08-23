9aa2afc12ab51a12d467eb1ec43e987b280ce9d5
gurujiathome <somwanshi.dnyaneshwar@gmail.com>
2017-08-23 14:57:53 +0530

    url changed

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -65728,7 +65728,7 @@ angular.module('mm.core')
     "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "",
+    "siteurl": "http://ec2-54-173-140-112.compute-1.amazonaws.com",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
     "privacypolicy": "https://moodle.org/mod/page/view.php?id=8148"
