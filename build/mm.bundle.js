45c846f682910aa86800b9bec8cea608e28451f6
gurujiathome <somwanshi.dnyaneshwar@gmail.com>
2018-01-10 12:17:24 +0530

    id changed

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -71101,9 +71101,9 @@ angular.module('mm.addons.mod_workshop')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.myprivateguru.moodlemobile",
+    "app_id" : "com.myprivateguru.mobile",
     "appname": "MyPrivateGuru",
-    "desktopappname": "Moodle Desktop",
+    "desktopappname": "MyGuru Desktop",
     "versioncode" : "2021",
     "versionname" : "3.4.0",
     "cache_expiration_time" : 300000,
